be3a25c752899f9a7d3e6a181286e0b2ca7e49e8
Alexandre Velloso <xande.velloso@gmail.com>
2018-06-05 18:31:12 -0300

    Arquivo LC melhorado

--- src/test/LC.java
+++ src/test/LC.java
@@ -9,19 +9,23 @@ public class LC {
 
     public static void main(String[] args) {
 
-        try {
-            String arquivoL = "exemplo2.l";
-            String arquivoASM = "C:\\Users\\alexandre\\Desktop\\MASM\\arquivo.asm";
-            
-            Programa.getInstance().readProgram("./Arquivos/Exemplos/" + arquivoL);
-            Sintatico.principal();
-            
-            // otimizacao
-            Otimizacao peephole = new Peephole( arquivoASM );
-            peephole.run();
-            
-        } catch (Exception e) {
-            e.printStackTrace();
+        if( args.length < 2 ){
+            System.out.println("ERRO: Você escreveu o comando errado, um exemplo é java LC arquivo.l arquivo.asm");
+        }else{
+            String arquivoL = args[0];
+            String arquivoAsm = args[1];
+
+            try{
+                Programa.getInstance().readProgram("./Arquivos/Exemplos/" + arquivoL);
+                Sintatico.principal();
+
+                // otimizacao
+                Otimizacao peephole = new Peephoke( arquivoAsm );
+                peephole.run();
+            }catch( Exception e ){
+                //e.printStackTrace();
+            }
         }
+        
     }
 }
